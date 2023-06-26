643ff7f3fab8b36da33350f62e7efcef17866d6a
Opiyo118 <opishegeofrey@gmail.com>
2023-06-26 11:56:03 -0700

    3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,14 +3,15 @@
 /**
  * _puts - prints a string to stdout
  * @str: pointer to the string to print
+ * Return: void
  */
-void _puts(char *str)
-{
-	int i;
 
+void _puts(char *str)
+	
+{
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(str[i]);
+	_putchar(str[i]);
 	}
 	_putchar('\n');
 }
